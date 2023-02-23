84aafed58459408822f1e9b221c75aff3e3b5e0a
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-02-23 22:43:17 +0530

    huh...here we go

--- jarvis.py
+++ jarvis.py
@@ -1,8 +1,41 @@
 import subprocess
+import datetime
 import os
 
-input = subprocess.getoutput("termux-speech-to-text")
-print("You said: "+str(input))
+# voice function!
+def VoiceFunction():
+    input = subprocess.getoutput("termux-speech-to-text")
+    print("You said:"+str(input))
+    if input == "hello":
+        os.system("termux-tts-speak {}".format("Hello I am Jarvis"))
+# text function!
+def TextFunction():
+    text = input("Your msg: ")
+    print(text)
 
-if input == "hello":
-   os.system("termux-tts-speak {}".format("Hello I am Jarvis"))
+# wishme function
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        os.system("termux-tts-speak {}".format("Good Morning"))
+    elif hour>=12 and hour<18:
+        os.system("termux-tts-speak {}".format("Good Morning"))
+    else:
+        os.system("termux-tts-speak {}".format("Good Evening"))
+    print("I am Jarvis Sir. Please tell me how may I help you")
+    selection()
+
+# def selection 
+def selection():
+    select = input("Please select which method you want to run as ur assistant..\n Voice or Text?: ")
+    # if user input == voice..
+    if select.lower() == "Voice":
+        print("voice!")
+        VoiceFunction()
+    # if user input == text..
+    elif select.lower() == "text":
+        print("text!")
+        TextFunction()
+
+if __name__ == "__main__":
+    wishMe()
